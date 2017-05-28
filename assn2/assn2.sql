0ffbbe4c74e58f331ed0259b62360887fe54274a
Teague Lander <tlander121@hotmail.com>
2017-05-28 00:17:42 -0700

    Added one last query and added comments

--- assn2/assn2.sql
+++ assn2/assn2.sql
@@ -18,56 +18,79 @@ CREATE OR REPLACE VIEW vCelebAlbumCounts AS
 	GROUP BY c.name;
 	
 -- SELECT STATEMENTS --
+
+--Find the movies where both Tom Cruise and Penelope Cruz have starred together
 SELECT movie
 	FROM vCostars
 	WHERE celeb1 = 'Tom Cruise'
 	AND celeb2 = 'Penélope Cruz';
 	
+--Find all the co-stars of Nicolas Cage
 SELECT UNIQUE(celeb2) celeb
 	FROM vCostars
 	WHERE celeb1 = 'Tom Cruise';
 	
+--Find the movies where Tom Cruise co-starred with a celebrity he is (or has been) in
+--a relationship with
 SELECT celeb2 costar, movie
 	FROM vCostars
 	NATURAL JOIN Relationships
 	WHERE celeb1 = 'Tom Cruise';
 	
+--Find the movies where a celebrity co-starred with another celebrity he/she is (or has
+--been) in relationship with
 SELECT celeb1, celeb2, movie
 	FROM vCostars
 	NATURAL JOIN Relationships;
-		
+
+--Find how many movies each celebrity has starred in. Order the results by the number
+--of movies (in descending order). Show only the celebrities who have starred in at
+--least 10 movies
 SELECT celeb, COUNT(movie)
 	FROM StarredIn
 	GROUP BY celeb
 	HAVING COUNT(movie) >= 10
 	ORDER BY COUNT(movie) DESC;
 	
+--Find the celebrities that have been in relationship with the same celebrity
 SELECT r1.celeb2 celeb1, r2.celeb2 celeb2, r1.celeb1 celeb3
 	FROM Relationships r1
 	JOIN Relationships r2 ON r1.celeb1 = r2.celeb1
 	WHERE r1.celeb2 <> r2.celeb2;
 	
+--For each pair of enemies give the number of movies each has starred in
 SELECT e.celeb1, e.celeb2, cmc1.movieCount, cmc2.movieCount
 	FROM Enemies e
 	JOIN vCelebMovieCounts cmc1 ON e.celeb1 = cmc1.celeb
 	JOIN vCelebMovieCounts cmc2 ON e.celeb2 = cmc2.celeb;
-	
+
+--Find how many albums each celebrity has released. Order the results by the number
+--of albums (in descending order). Show only the celebrities who have released at least
+--2 albums
 SELECT celeb, albumCount
 	FROM vCelebAlbumCounts
 	WHERE albumCount >= 2
 	ORDER BY albumCount DESC;
 	
--- This is somewhat ambiguous, I'm assuming 'some' means more than one is allowed
+--Find those celebrity that have starred in some movie and have released some album
 SELECT celeb
 	FROM vCelebAlbumCounts
 	NATURAL JOIN vCelebMovieCounts
 	WHERE movieCount > 0
 	AND albumCount > 0;
 	
+--For each celebrity that has both starred in some movie and released some album give
+--the numbers of movies and albums he/she has starred in and released, respectively
 SELECT celeb, movieCount number_of_movies, albumCount number_of_albums
 	FROM vCelebAlbumCounts
 	NATURAL JOIN vCelebMovieCounts
 	WHERE movieCount > 0
 	AND albumCount > 0;
 	
+--Find the earliest and the latest relationship (w.r.t the start date) recorded in this
+--database
+SELECT *
+	FROM Relationships
+	WHERE started IN ((SELECT MAX(started) FROM Relationships),
+					  (SELECT MIN(started) FROM Relationships));
 
